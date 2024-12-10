8810e2f8bca52900ffb9606b6ddd9bebcacd018d
MalikT <maliktremain1@gmail.com>
2024-12-10 08:33:26 +0000

    boltzmann more complicated than thought

--- src/Current/Genetics.hpp
+++ src/Current/Genetics.hpp
@@ -17,32 +17,76 @@
 #include <ranges>
 
 
-template<size_t population_size, FileDataType file_data_type>
+template<size_t POPULATION_SIZE, FileDataType FILE_DATA_TYPE>
 class GeneticAlgorithm
 {
 public:
-    explicit GeneticAlgorithm(std::reference_wrapper<GenPar const>&& gp, FileData<file_data_type>& data):
+    explicit GeneticAlgorithm(std::reference_wrapper<GenPar const>&& gp, FileData<FILE_DATA_TYPE>& data):
         mt_(std::random_device{}()),
         gen_par_(gp),
         bands_(InitBands()),
-        data_(data)
+        data_(data),
+        //using the top 5 % of a population as the elites
+        elite_count_(static_cast<uint64_t>(std::ceil(static_cast<double>(POPULATION_SIZE) * 0.05))),
+        temperature_cooling_rate_(0.95),
+        temperature_(10.0),
+        boltzmann_tournament_size_(2)
     {
-        static_assert(population_size > 5, "Population size must be greater than 0");
+        static_assert(POPULATION_SIZE > 5, "Population size must be greater than 0");
     }
     void EvaluatePopulation()
     {
         std::ranges::for_each(bands_,[this](BandConfig& band){band.SetFitnessScore(FindCompressionRatio(data_,band,gen_par_));});
         std::ranges::sort(bands_,[](BandConfig const& band1, BandConfig const& band2) -> bool {return band1.GetFitnessScore() > band2.GetFitnessScore();});
     }
-    std::vector<std::pair<BinString, BinString>>&& SelectFromPopulation()
-    {
-        //population_size >> 1 approximates half population for the reserve
-        std::vector<std::pair<BinString, BinString>> results(population_size >> 1);
-        //elitism, using saving best 2.
-        results.emplace_back({{bands_[0], gen_par_},{bands_[1], gen_par_}});
 
+    std::vector<double>&& CalcBoltzmannProbablities(std::vector<BandConfig const*> const& selected_participants) const
+    {
+        std::vector<double> boltzmannProbabilities(boltzmann_tournament_size_);
+        double total{};
+        for (size_t i = 0; i < selected_participants.size(); ++i)
+        {
+            total += std::exp(selected_participants[i]->GetFitnessScore() / temperature_);
+        }
+        for (size_t i = 0; i < selected_participants.size(); ++i)
+        {
+            boltzmannProbabilities[i] = std::exp(selected_participants[i]->GetFitnessScore() / temperature_) / total;
+        }
+        return std::move(boltzmannProbabilities);
+    }
+    //can't mark const since it changes the mersenne twister value
+    std::pair<BandConfig const&, BandConfig const&>&& PickPairFromBoltzmannTournamentSelection()
+    {
+        std::uniform_int_distribution<size_t> participant_picker(0,POPULATION_SIZE-1);
+        std::vector<BandConfig const*> participants;
+        for (size_t i = 0; i < boltzmann_tournament_size_; ++i)
+        {
+            participants.emplace_back(&bands_[participant_picker(i)]);
+        }
+        std::vector<double> const boltzmannProbabilities = CalcBoltzmannProbablities(participants);
+        std::discrete_distribution<> parent_picker(boltzmannProbabilities.cbegin(), boltzmannProbabilities.cend());
+        return {*participants[parent_picker(mt_)], *participants[parent_picker(mt_)]};
     }
 
+    /**
+     * TODO: Store elites in new pop and remove from selection, fill new pop while < current_pop.size() with
+     * TODO: offspring made by parents selected from the boltzmann method
+     * TODO: then move over the values in new pop to current pop
+     */
+    void GenerateNewPopulation()
+    {
+        std::vector<BandConfig> NewPopulation;
+        //assumes sorted population
+
+        auto idx_seq = std::make_index_sequence<POPULATION_SIZE> { };
+        for (size_t i = 0; i < elite_count_; ++i)
+        {
+            //doing a "dumb" implamentation for now, might wanna order by asc order
+            //to later be able to do vector.back() and .pop()
+            NewPopulation.emplace_back(std::move(*std::ranges::max_element(bands_)));
+        }
+
+    }
     /**
      * rough sketch of the pipeline
      * BC = BandConfig
@@ -56,6 +100,9 @@ public:
      * ConvertAll(BS):BC ->
      * GOTO _LOOP
      *
+     */
+    /**
+     * Single-point cross over
      */
     void CrossOver(BinString& a, BinString& b)
     {
@@ -71,20 +118,34 @@ public:
         a_data ^= difference_mask & cross_over_mask;
         b_data ^= difference_mask & cross_over_mask;
     }
+
+    /**
+     * Starting with simple mutation strategy
+     * @param bs
+     */
     void Mutate(BinString& bs)
     {
-
+        auto const active_bits_zero_indexed = gen_par_.get().Get(GenPar::Params::BitWidth) - 2;
+        auto const mutation_point = std::uniform_int_distribution<> {0U, active_bits_zero_indexed}(mt_);
+        uint64_t& bs_data = bs.GetData();
+        bs_data ^= 1ULL << mutation_point;
     }
     void Run()
     {
         //population inited by ctor
-        auto&& new_pop = SelectFromPopulation();
+        std::vector<BinString> new_bands(POPULATION_SIZE);
+        // auto&& new_pop = SelectFromPopulation();
+        for (auto const& band : bands_)
+        {
+            new_bands.emplace_back(band, gen_par_.get());
+        }
         while (true)
         {
             // CrossOver(new_pop.first, new_pop.second);
             Mutate();
             EvaluatePopulation();
-            new_pop = SelectFromPopulation();
+            GenerateNewPopulation();
+            temperature_ *= temperature_cooling_rate_;
         }
     }
     void PrintPopulation()
@@ -112,20 +173,24 @@ private:
     }
 
     template <std::size_t... Indices>
-    std::array<BandConfig, population_size> InitBandsImpl(std::index_sequence<Indices...>)
+    std::array<BandConfig, POPULATION_SIZE> InitBandsImpl(std::index_sequence<Indices...>)
     {
         return { (static_cast<void>(Indices), GenerateRandomBand())... };
     }
 
-    std::array<BandConfig, population_size> InitBands()
+    std::array<BandConfig, POPULATION_SIZE> InitBands()
     {
-        return InitBandsImpl(std::make_index_sequence<population_size>{});
+        return InitBandsImpl(std::make_index_sequence<POPULATION_SIZE>{});
     }
 private:
     std::mt19937 mt_;
     std::reference_wrapper<GenPar const> gen_par_;
-    std::array<BandConfig, population_size> bands_;
-    FileData<file_data_type>& data_;
+    std::array<BandConfig, POPULATION_SIZE> bands_;
+    FileData<FILE_DATA_TYPE>& data_;
+    uint64_t const elite_count_;
+    double const temperature_cooling_rate_;
+    double temperature_;
+    size_t const boltzmann_tournament_size_;
 };
 
 
